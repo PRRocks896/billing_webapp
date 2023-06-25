55280cb1ea22c35e824d15b222866b90ac0e9e6f
Darshan1606 <darshantarsariya17@gmail.com>
2023-06-25 10:47:05 +0530

    master sub menu : add tables and header

--- src/pages/Customer/index.js
+++ src/pages/Customer/index.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
-import { Box, Button, Grid, IconButton, InputAdornment, InputBase, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@mui/material'
-import { FiEdit3, FiPlus, FiSearch, FiTrash2 } from 'react-icons/fi'
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputBase,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
+import { FiEdit3, FiPlus, FiSearch, FiTrash2 } from "react-icons/fi";
 
-const projects = [
-  {id:1,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:2,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:3,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:4,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:5,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:6,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:7,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:8,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:9,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:10,name:"Krushang rathod",number:9879854706,gender:"Male"},
-  {id:11,name:"Krushang rathod",number:9879854706,gender:"Male"},
-]
+const customers = [
+  { id: 1, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 2, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 3, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 4, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 5, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 6, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 7, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 8, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 9, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 10, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+  { id: 11, name: "Krushang rathod", number: 9879854706, gender: "Male" },
+];
 
 const Customer = () => {
-
   // pagination code start
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -32,104 +45,110 @@ const Customer = () => {
   };
 
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - projects.length) : 0;
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - customers.length) : 0;
 
   const visibleRows = React.useMemo(
-    () =>
-      (projects).slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage,
-      ),
-    [page, rowsPerPage],
+    () => customers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [page, rowsPerPage]
   );
   // pagination code end
 
   return (
     <>
       {/* top page action with text */}
-      <Box className='top-bar'>
-        <Grid container justifyContent={'space-between'} alignItems={'center'}>
+      <Box className="top-bar">
+        <Grid container justifyContent={"space-between"} alignItems={"center"}>
           <Grid item>
-            <Box className='search-box'>
-              <InputBase 
-                  name="customer-search"
-                  placeholder="Search customer"
-                  endAdornment={
-                    <InputAdornment position="end" className='end-input-icon text-grey'>
-                      <IconButton aria-label="toggle password visibility" edge="end">
-                        <FiSearch />
-                      </IconButton>
-                    </InputAdornment>
-                  }
+            <Box className="search-box">
+              <InputBase
+                name="customer-search"
+                placeholder="Search customer"
+                endAdornment={
+                  <InputAdornment
+                    position="end"
+                    className="end-input-icon text-grey"
+                  >
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      edge="end"
+                    >
+                      <FiSearch />
+                    </IconButton>
+                  </InputAdornment>
+                }
               />
             </Box>
           </Grid>
-          
+
           <Grid item>
-            <Button component={'button'} className='btn btn-tertiary'>
-              <FiPlus /> &nbsp; Add Customer
+            <Button component={"button"} className="btn btn-tertiary">
+              <FiPlus /> &nbsp; <p>Add Customer</p>
             </Button>
-          </Grid>        
+          </Grid>
         </Grid>
       </Box>
-      
-      {/* project listing */}
-      <Box className='card'>
+
+      {/* customer listing */}
+      <Box className="card">
         <Box className="">
-          <TableContainer className='table-wrapper'>
+          <TableContainer className="table-wrapper">
             <Table>
-                <TableHead>
-                    <TableRow>
-                      <TableCell>No</TableCell>
-                      <TableCell>Name</TableCell>
-                      <TableCell>Phone</TableCell>
-                      <TableCell>Gender</TableCell>
-                      <TableCell>Action</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {visibleRows.length ? 
-                      visibleRows.map((row, index)=>{
-                        return<>
-                            <TableRow key={row.id}>
-                              <TableCell align='left'>{index + 1}</TableCell>
-                              <TableCell align='left'>{row.name}</TableCell>
-                              <TableCell align='left'>{row.number}</TableCell>
-                              <TableCell align='left'>{row.gender}</TableCell>
-                              <TableCell>
-                                  <Box className='table-action-btn'>  
-                                    <Button className='btn btn-primary'>
-                                        <FiEdit3/>
-                                    </Button>
-                                    <Button className='btn btn-primary' >
-                                        <FiTrash2/>
-                                    </Button>
-                                  </Box>
-                              </TableCell>
-                            </TableRow>
-                          </>
-                        })
-                        : 
-                        <TableRow>
-                          <TableCell sx={{textAlign:'center'}} colSpan={7}>No Projects Found</TableCell>
+              <TableHead>
+                <TableRow>
+                  <TableCell>No</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Phone</TableCell>
+                  <TableCell>Gender</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {visibleRows.length ? (
+                  visibleRows.map((row, index) => {
+                    return (
+                      <>
+                        <TableRow key={row.id}>
+                          <TableCell align="left">{index + 1}</TableCell>
+                          <TableCell align="left">{row.name}</TableCell>
+                          <TableCell align="left">{row.number}</TableCell>
+                          <TableCell align="left">{row.gender}</TableCell>
+                          <TableCell>
+                            <Box className="table-action-btn">
+                              <Button className="btn btn-primary">
+                                <FiEdit3 />
+                              </Button>
+                              <Button className="btn btn-primary">
+                                <FiTrash2 />
+                              </Button>
+                            </Box>
+                          </TableCell>
                         </TableRow>
-                    } 
-                    {emptyRows > 0 && (
-                      <Box
-                        style={{
-                          height: 53 * emptyRows,
-                        }}
-                      >
-                        <TableCell colSpan={6} />
-                      </Box>
-                    )}
-                </TableBody>             
+                      </>
+                    );
+                  })
+                ) : (
+                  <TableRow>
+                    <TableCell sx={{ textAlign: "center" }} colSpan={7}>
+                      No customers Found
+                    </TableCell>
+                  </TableRow>
+                )}
+                {emptyRows > 0 && (
+                  <Box
+                    style={{
+                      height: 53 * emptyRows,
+                    }}
+                  >
+                    <TableCell colSpan={6} />
+                  </Box>
+                )}
+              </TableBody>
             </Table>
           </TableContainer>
           <TablePagination
             rowsPerPageOptions={[5, 10, 25]}
             component="div"
-            count={projects.length}
+            count={customers.length}
             rowsPerPage={rowsPerPage}
             page={page}
             onPageChange={handleChangePage}
@@ -137,9 +156,8 @@ const Customer = () => {
           />
         </Box>
       </Box>
-
     </>
-  )
-}
+  );
+};
 
-export default Customer
+export default Customer;
--- src/pages/PaymentType/index.js
+++ src/pages/PaymentType/index.js
@@ -1,9 +1,160 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputBase,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
+import { FiEdit3, FiPlus, FiSearch, FiTrash2 } from "react-icons/fi";
+
+const paymentType = [
+  { id: 1, pType: "payment type" },
+  { id: 2, pType: "payment type" },
+  { id: 3, pType: "payment type" },
+  { id: 4, pType: "payment type" },
+  { id: 5, pType: "payment type" },
+  { id: 6, pType: "payment type" },
+  { id: 7, pType: "payment type" },
+  { id: 8, pType: "payment type" },
+  { id: 9, pType: "payment type" },
+  { id: 10, pType: "payment type" },
+  { id: 11, pType: "payment type" },
+];
 
 const PaymentType = () => {
-  return (
-    <div>PaymentType</div>
-  )
-}
+  // pagination code start
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-export default PaymentType
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - paymentType.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      paymentType.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [page, rowsPerPage]
+  );
+  // pagination code end
+
+  return (
+    <>
+      {/* top page action with text */}
+      <Box className="top-bar">
+        <Grid container justifyContent={"space-between"} alignItems={"center"}>
+          <Grid item>
+            <Box className="search-box">
+              <InputBase
+                name="paymentType-search"
+                placeholder="Search Payment Type"
+                endAdornment={
+                  <InputAdornment
+                    position="end"
+                    className="end-input-icon text-grey"
+                  >
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      edge="end"
+                    >
+                      <FiSearch />
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </Box>
+          </Grid>
+
+          <Grid item>
+            <Button component={"button"} className="btn btn-tertiary">
+              <FiPlus /> &nbsp; <p>Add Payment Type</p>
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+
+      {/* payment type listing */}
+      <Box className="card">
+        <Box className="">
+          <TableContainer className="table-wrapper">
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>No</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {visibleRows.length ? (
+                  visibleRows.map((row, index) => {
+                    return (
+                      <>
+                        <TableRow key={row.id}>
+                          <TableCell align="left">{index + 1}</TableCell>
+                          <TableCell align="left">{row.pType}</TableCell>
+                          <TableCell>
+                            <Box className="table-action-btn">
+                              <Button className="btn btn-primary">
+                                <FiEdit3 size={15} />
+                              </Button>
+                              <Button className="btn btn-primary">
+                                <FiTrash2 size={15} />
+                              </Button>
+                            </Box>
+                          </TableCell>
+                        </TableRow>
+                      </>
+                    );
+                  })
+                ) : (
+                  <TableRow>
+                    <TableCell sx={{ textAlign: "center" }} colSpan={7}>
+                      No PaymentType Found
+                    </TableCell>
+                  </TableRow>
+                )}
+                {emptyRows > 0 && (
+                  <Box
+                    style={{
+                      height: 53 * emptyRows,
+                    }}
+                  >
+                    <TableCell colSpan={6} />
+                  </Box>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={paymentType.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default PaymentType;
