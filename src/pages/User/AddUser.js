98b7a33068defe3c1565a4b05b1449d7928155b3
RK <kushangrathod5@gmail.com>
2023-06-29 21:18:40 +0530

    create add user form using react-hook-form

--- src/pages/User/AddUser.js
+++ src/pages/User/AddUser.js
@@ -63,51 +63,221 @@ const AddUser = () => {
           <Button className="btn btn-cancel">Cancel</Button>
         </Grid>
       </Grid> */}
-
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          placeholder="Last name"
-          {...register("Last name", { required: true, maxLength: 100 })}
-        />
-        {/* <input
-          type="text"
-          placeholder="First name"
-          {...register("First name", { required: true, maxLength: 80 })}
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
-        />
-        <input
-          type="tel"
-          placeholder="Mobile number"
-          {...register("Mobile number", {
-            required: true,
-            minLength: 6,
-            maxLength: 12,
-          })}
-        />
-        <select {...register("Title", { required: true })}>
-          <option value="Mr">Mr</option>
-          <option value="Mrs">Mrs</option>
-          <option value="Miss">Miss</option>
-          <option value="Dr">Dr</option>
-        </select>
+        <Box className="card">
+          <Box className="top-bar">
+            <Grid container justifyContent={"end"}>
+              <Grid item md={0.7}>
+                <Button className="btn-close" onClick={() => navigate(-1)}>
+                  <FiX size={25} color="var(--color-grey)" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
 
-        <input
-          {...register("Developer", { required: true })}
-          type="radio"
-          value="Yes"
-        />
-        <input
-          {...register("Developer", { required: true })}
-          type="radio"
-          value="No"
-        /> */}
+          <FormGroup className="form-field">
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={
+                      !errors.first_name ? "input-field" : "border-error"
+                    }
+                  >
+                    <input
+                      type="text"
+                      placeholder="First name"
+                      {...register("first_name", {
+                        required: true,
+                        maxLength: 100,
+                      })}
+                    />
+                  </div>
+                  {errors.first_name && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={
+                      !errors.last_name ? "input-field" : "border-error"
+                    }
+                  >
+                    <input
+                      type="text"
+                      placeholder="Last name"
+                      {...register("last_name", {
+                        required: true,
+                        maxLength: 100,
+                      })}
+                    />
+                  </div>
+                  {errors.last_name && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={
+                      !errors.username ? "input-field" : "border-error"
+                    }
+                  >
+                    <input
+                      type="text"
+                      placeholder="Username"
+                      {...register("username", {
+                        required: true,
+                        maxLength: 100,
+                      })}
+                    />
+                  </div>
+                  {errors.username && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={!errors.role ? "input-field" : "border-error"}
+                  >
+                    <select
+                      {...register("role", {
+                        required: true,
+                        maxLength: 100,
+                      })}
+                    >
+                      <option value="">Select Role</option>
+                      <option value="1">Role 1</option>
+                      <option value="2">Role 2</option>
+                      <option value="3">Role 3</option>
+                    </select>
+                  </div>
+                  {errors.role && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+              <Grid item xs={8}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={!errors.branch ? "input-field" : "border-error"}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Branch name"
+                      {...register("branch", {
+                        required: true,
+                        maxLength: 100,
+                      })}
+                    />
+                  </div>
+                  {errors.branch && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={!errors.email ? "input-field" : "border-error"}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Email address"
+                      {...register("email", {
+                        required: true,
+                        maxLength: 100,
+                        pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                      })}
+                    />
+                  </div>
+                  {errors.email && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={!errors.phone ? "input-field" : "border-error"}
+                  >
+                    <input
+                      type="number"
+                      placeholder="+91"
+                      {...register("phone", {
+                        required: true,
+                        maxLength: 10,
+                        minLength: 10,
+                      })}
+                    />
+                  </div>
+                  {errors.phone && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <FormControl variant="standard" className="form-control">
+                  <div
+                    className={!errors.address ? "input-field" : "border-error"}
+                  >
+                    <textarea
+                      type="text"
+                      rows={3}
+                      placeholder="Address"
+                      {...register("address", {
+                        required: true,
+                        maxLength: 250,
+                      })}
+                    />
+                  </div>
+                  {errors.address && (
+                    <span style={{ fontSize: "14px", color: "red" }}>
+                      This field is required
+                    </span>
+                  )}
+                </FormControl>
+              </Grid>
+            </Grid>
+          </FormGroup>
+        </Box>
 
-        <input type="submit" />
+        <Grid container spacing={3} sx={{ marginTop: "6px" }}>
+          <Grid item md={1.5}>
+            <Button type="submit" className="btn btn-tertiary">
+              Save
+            </Button>
+            {/* <input type="submit" value={"Save"} className="btn btn-tertiary" /> */}
+          </Grid>
+          <Grid item md={1.5}>
+            <Button className="btn btn-cancel">Cancel</Button>
+          </Grid>
+        </Grid>
       </form>
     </>
   );
