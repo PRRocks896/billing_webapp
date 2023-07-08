3dc696445fe38dc88a182b77a5836cfb5d4c2ba2
RK <kushangrathod5@gmail.com>
2023-07-08 19:46:31 +0530

    change loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,8 @@
 import React from "react";
-import CircularProgress from "@mui/material/CircularProgress";
+// import CircularProgress from "@mui/material/CircularProgress";
 import Backdrop from "@mui/material/Backdrop";
 import { useSelector } from "react-redux";
+import "../assets/styles/loader.scss";
 
 const Loader = () => {
   const { isLoading } = useSelector((state) => state.loader);
@@ -11,7 +12,13 @@ const Loader = () => {
       sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
       open={isLoading}
     >
-      <CircularProgress />
+      {/* <CircularProgress
+        disableShrink={false}
+        thickness={2}
+        size={70}
+        color="#000"
+      /> */}
+      <span class="loader"></span>
     </Backdrop>
   );
 };
