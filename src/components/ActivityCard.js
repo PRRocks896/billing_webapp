5cbc42e455a25701dcee8966bca96b048e0e54ad
RK <kushangrathod5@gmail.com>
2023-07-31 21:24:14 +0530

    resolve print bill and card navigation

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const ActivityCard = ({
   ActivityTitle,
@@ -9,15 +10,16 @@ const ActivityCard = ({
   ActivityIcon,
   path,
 }) => {
+  const navigate = useNavigate();
   return (
     <>
       <Grid item xs={12} sm={6} md={6} lg={3} xxl={3}>
-        <Box className="activity-card">
+        <Box className="activity-card" onClick={() => navigate(path)}>
           <Box className="activity-content_1">
             <Typography variant="h6" component="h6" className="text-white">
               {ActivityTitle}
             </Typography>
-            <Link className="arrow-icon" to={path}>
+            <Link className="arrow-icon">
               <FaLongArrowAltRight />
             </Link>
           </Box>
