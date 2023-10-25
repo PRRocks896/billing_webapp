93691a715f11db692463fd6ed0888dd7351cf9ac
PRRocks896 <pratikparmar896@gmail.com>
2023-10-25 18:20:49 +0530

    [SETUP] PWA functionality

--- src/sw.js
+++
@@ -1,7 +0,0 @@
-// sw.js
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
-
-workbox.routing.registerRoute(
-  new RegExp('.*'),
-  new workbox.strategies.NetworkFirst()
-);
