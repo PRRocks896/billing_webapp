ea8dbe45d354984b6a6c9b50254499f814a8f983
prrocks896 <pratikparmar896@gmail.com>
2025-05-01 22:22:54 +0530

    [ADD] add dateChange hook

--- src/hook/useDateChangeChecker.js
+++ src/hook/useDateChangeChecker.js
@@ -6,22 +6,22 @@ export const useDateChangeChecker = (interval = 60000) => {
     const [dateMismatch, setDateMismatch] = useState(false);
 
     useEffect(() => {
-        // const checkDate = () => {
-        //     const currentDate = getCurrentDate();
-        //     const serverDate = localStorage.getItem('serverDate');
+        const checkDate = () => {
+            const currentDate = getCurrentDate();
+            const serverDate = localStorage.getItem('serverDate');
 
-        //     if (serverDate && serverDate !== currentDate) {
-        //         setDateMismatch(true);
-        //         localStorage.removeItem("managerId");
-        //         localStorage.removeItem("managerName");
-        //         localStorage.removeItem('serverDate');
-        //     }
-        // };
+            if (serverDate && serverDate !== currentDate) {
+                setDateMismatch(true);
+                localStorage.removeItem("managerId");
+                localStorage.removeItem("managerName");
+                localStorage.removeItem('serverDate');
+            }
+        };
 
-        // checkDate(); // Initial check
-        // const timer = setInterval(checkDate, interval);
+        checkDate(); // Initial check
+        const timer = setInterval(checkDate, interval);
 
-        // return () => clearInterval(timer); // Cleanup on unmount
+        return () => clearInterval(timer); // Cleanup on unmount
     }, [interval]);
 
     return dateMismatch;
